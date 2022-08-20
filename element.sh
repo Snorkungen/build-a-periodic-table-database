5e13f6c3f7ee5b4369c5d8d3bb963d21fe5ff19d
Snorkungen <teuvo2004@gmail.com>
2022-08-20 08:34:23 +0000

    Initial commit

---
+++ element.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+
+if [[ -z $1 ]];then
+  echo "Please provide an element as an argument."
+else 
+  echo "Hello, $1"
+fi
